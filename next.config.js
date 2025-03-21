d3400525fc4ce0d2daad72a6f9a6df9a5b5cafdb
Sharath <sharathsudheer2310@gmail.com>
2025-03-21 17:01:10 +0530

    Ignore TypeScript errors during build

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     // Ignore ESLint errors during build
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Ignore TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig 
